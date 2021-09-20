b7b8d05b106478d5dba71aa17123a7e9363a6cb4
Jason Banahan <banahanjason@gmail.com>
2021-09-20 14:01:04 -0500

    Change styles to make page more readable
    And some other minor changes from having to re-record the video

--- week6/js/index.js
+++ week6/js/index.js
@@ -1,12 +1,12 @@
-document.write("<p>hello from external declaration!</p>");
+document.write("<p>Hello from external declaration!</p>");
 
 var name;
 var yearOfBirth;
 
-name = prompt("What is your name?");
+name = prompt("Enter your name");
 yearOfBirth = prompt("What year were you born?");
 
 yearOfBirth = parseInt(yearOfBirth);
 
 var age = 2021 - yearOfBirth;
-document.write("<h2>Hello <em>" + name + "</em>, you are <em>" + age + "</em> years old</h2>");
+document.write("<h3>Hello <em>" + name + "</em>, you are <em>" + age + "</em> years old.</h3>");
